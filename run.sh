387e00885e42074f11501f1fbf4f50465f6cb38d
root <root@virthost01.skyline.lan>
2022-09-14 13:07:10 +0100

    localdocker

--- run.sh
+++ run.sh
@@ -1,11 +1,21 @@
-docker build --no-cache . -t stevenblack-hosts
+docker build --no-cache -t stevenblack-hosts .
 
 #docker run --rm -it stevenblack-hosts updateHostsFile.py
 
+# #docker run \
+#     --pull always \
+#     --rm -it \
+#     -v "$(pwd)/hosts:/etc/hosts" \
+#     -v "$(pwd)/whitelist:/hosts/whitelist" \
+#     ghcr.io/stevenblack/hosts:latest updateHostsFile.py \
+#     --auto --replace --extensions gambling porn
+
 docker run \
-    --pull always \
     --rm -it \
     -v "$(pwd)/hosts:/etc/hosts" \
     -v "$(pwd)/whitelist:/hosts/whitelist" \
-    ghcr.io/stevenblack/hosts:latest updateHostsFile.py \
+    stevenblack-hosts updateHostsFile.py \
     --auto --replace --extensions gambling porn
+
+    #stevenblack-hosts
+    #ghcr.io/stevenblack/hosts:latest updateHostsFile.py \
