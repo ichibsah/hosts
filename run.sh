5c16678fbf39290f066f7e9a64812c737afa3d7e
root <root@virthost01.skyline.lan>
2022-09-16 10:28:52 +0100

    my ext

--- run.sh
+++ run.sh
@@ -15,7 +15,8 @@ docker run \
     -v "$(pwd)/hosts:/etc/hosts" \
     -v "$(pwd)/whitelist:/hosts/whitelist" \
     stevenblack-hosts updateHostsFile.py \
-    --auto --replace --extensions gambling porn fakenews social
+    --auto --replace --extensions others
+    #--auto --replace --extensions gambling porn fakenews social
 
     #stevenblack-hosts
     #ghcr.io/stevenblack/hosts:latest updateHostsFile.py \
